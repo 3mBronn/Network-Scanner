5db745a85d45df3b1e9977c3407641c57d10fb0b
Emre <114446832+3mBronn@users.noreply.github.com>
2023-10-10 23:30:00 +0300

    Add files via upload

---
+++ networkscan.py
@@ -0,0 +1,33 @@
+import scapy.all as scapy
+import optparse
+
+
+def get_user_info():
+    object_parse = optparse.OptionParser()
+    object_parse.add_option("-i", "--ipadd",dest = "ipadd" ,help="Enter IP address an its prefix length")
+
+    (user_input, arguments) = object_parse.parse_args()
+
+    if not user_input.ipadd:
+        parser.error("[-]Please specify an IP address and prefix length. Use --help for more information")
+    
+    
+    return user_input    
+
+
+def net_scan(ip):
+    arp_request = scapy.ARP(pdst = ip)
+
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+    combine = broadcast/arp_request
+    (answered_list, unanswered_list) = scapy.srp(combine, timeout=1)
+
+    answered_list.summary()
+
+
+print("[+]Network scanner has started!")
+
+
+user_ip_add = get_user_info()
+
+net_scan(user_ip_add.ipadd)
